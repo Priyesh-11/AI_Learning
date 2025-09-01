7e8a472339e4eb880a656e300fa5e3e26f35ffbd
Priyesh <priyeshwas11@gmail.com>
2025-09-01 11:03:39 +0530

    Add implementation to check the type of variables assigned using input() function

---
+++ Chapter_2/Problem_03.py
@@ -0,0 +1,6 @@
+#  Check the type of variable assigned using input () function. 
+
+a = int(input("Enter first number: "))
+b = input("Enter second number: ")
+print("The type of variable a is: ", type(a))
+print("The type of variable b is: ", type(b))
