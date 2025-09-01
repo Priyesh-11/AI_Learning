a780fe1fcdf9191d23323b681a98a5ccaecf87ca
Priyesh <priyeshwas11@gmail.com>
2025-09-01 10:55:48 +0530

    Add initial implementations for variable and data type examples in Chapter 2

---
+++ Chapter_2/01_variable.py
@@ -0,0 +1,10 @@
+a= 10      
+# variables = container to store a value. 
+b= "last breath" # keywords = reserved words in python  
+c= 56.22   # identifiers = class/function/variable name
+
+print(a)
+print(b)    
+print(c)
+
+print(a+c)
