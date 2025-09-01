ad982b739d3228b9bb4227521a4dabcce7dfd8bd
Priyesh <priyeshwas11@gmail.com>
2025-09-01 11:01:50 +0530

    Add implementation of a program to find the remainder of two numbers

---
+++ Chapter_2/Problem_02.py
@@ -0,0 +1,7 @@
+# Write a python program to find remainder when a number is divided by z.
+
+a = int(input("Enter first number: "))
+b = int(input("Enter second number: "))
+
+c = a % b
+print("The remainder when first number is divided by second number is: ", c)
