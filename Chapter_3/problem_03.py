52f23fd939c0f341713542d1a5828cdc49041db5
Priyesh <priyeshwas11@gmail.com>
2025-09-04 15:03:56 +0530

    Add program to detect double spaces in a string

---
+++ Chapter_3/problem_03.py
@@ -0,0 +1,5 @@
+# Write a program to detect double space in a string.
+
+a = "priyesh is a good guy  but sometimes he is a bad guy"
+
+print(a.find("  "))  # find function returns the index of the first occurrence of the substring. If not found, it returns -1.
