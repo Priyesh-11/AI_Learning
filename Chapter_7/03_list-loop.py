ee891123aa5608ee6aee5cbb467931e70986c311
Priyesh <priyeshwas11@gmail.com>
2025-09-12 12:34:01 +0530

    Add list and tuple iteration examples with print statements

---
+++ Chapter_7/03_list-loop.py
@@ -0,0 +1,9 @@
+list = ["priyesh", "sachin", "rahul", "rohit", "virat" ]
+for item in list:
+    print(item)  # priyesh sachin rahul rohit virat
+
+print("\n")
+
+tuple = ("priyesh", "sachin", "rahul", "rohit", "virat" )
+for item in tuple:
+    print(item)  # priyesh sachin rahul rohit virat
