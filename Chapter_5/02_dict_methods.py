4ef643179870cc5284dca0f31118e3af24b317e7
Priyesh <priyeshwas11@gmail.com>
2025-09-12 12:03:35 +0530

    Add initial implementations for dictionary and set operations, including user input handling

---
+++ Chapter_5/02_dict_methods.py
@@ -0,0 +1,15 @@
+marks = {
+    "Priyesh": 100,
+    "Shubham": 56,
+    "Rohan": 23,
+    0: "Harry"
+}
+
+# print(marks.items())
+# print(marks.keys())
+# print(marks.values())
+# marks.update({"Harry": 99, "Renuka": 100})
+# print(marks)
+
+print(marks.get("Priyesh2")) # Prints None
+print(marks["Priyesh2"]) # Returns an error
