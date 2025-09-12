9831f425374fd901a89ff93d828fd8d1c8cb6ce3
Priyesh <priyeshwas11@gmail.com>
2025-09-12 12:38:25 +0530

    Add string iteration example to print each character in a string

--- Chapter_7/03_list-loop.py
+++ Chapter_7/03_list-loop.py
@@ -7,3 +7,8 @@ print("\n")
 tuple = ("priyesh", "sachin", "rahul", "rohit", "virat" )
 for item in tuple:
     print(item)  # priyesh sachin rahul rohit virat
+
+
+str= "priyesh is a good boy"
+for x in str:
+    print(x)  # prints each character in the string
