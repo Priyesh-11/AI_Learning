f57716a92e888401738087ee067284f2cdaeeaf2
Priyesh <priyeshwas11@gmail.com>
2025-09-12 12:16:15 +0530

    Add while loop implementation to print a message 1101 times

---
+++ Chapter_7/01_loops.py
@@ -0,0 +1,14 @@
+# print priyesh is a good boy 1101 times
+
+# for i in range(1,1102):
+#     print(f"priyesh is a good boy {i} times")
+
+# # print 1 to 1101
+
+
+#using while loop
+i =1
+while i <= 1101:
+    print(f"priyesh is a good boy {i} times")
+    i += 1
+
